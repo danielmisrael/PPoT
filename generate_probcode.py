13c9577e914e6549ce49df0b219140b027a0fad2
Renato Lui Geh <renatogeh@gmail.com>
2025-08-08 21:38:32 -0700

    Construct probabilistic programs from logits

--- generate_probcode.py
+++ generate_probcode.py
@@ -1,6 +1,6 @@
 import re
 import torch, transformers
-import program
+import program, utils
 
 def extract_code(response_str):
     """Extract code from response string"""
@@ -27,33 +27,48 @@ def compile_probcode(code, probabilities):
 
 
 def get_probs(token_ids: torch.LongTensor, pos: list, logits: torch.FloatTensor,
-              processor: transformers.AutoProcessor) -> tuple:
+              processor: transformers.AutoProcessor, supp: list = None) -> tuple:
     '''
     Inputs:
         token_ids: torch.LongTensor of shape (batch_size, sequence_length)
         pos: list of lists containing the position of all random variable tokens
         logits: torch.FloatTensor of shape (batch_size, sequence_length, vocab_size)
         processor: the model's transformers.AutoProcessor
+        supp (optional): a list of torch.LongTensor containing the support (as token ids) of each
+            random variable. If not given, assume digits; if supp is a one dimensional
+            torch.LongTensor, then assume all variables have same support.
     Returns:
         A list of probabilistic programs of type program.Program
-        The loglikelihood of each program
+        The normalized loglikelihood of each program
     '''
+    if supp is None:
+        S = processor.tokenizer([str(i) for i in range(10)], return_tensors="pt").input_ids.flatten()
+        supp = [S for _ in range(token_ids.shape[0])]
+    elif torch.is_tensor(supp): supp = [supp for _ in range(token_ids.shape[0])]
+
     PP = []
 
-    for T, P, L in zip(token_ids, pos, logits):
+    # Compute loglikelihoods.
+    M = torch.isin(token_ids, torch.tensor(processor.tokenizer.all_special_ids)) # special tokens
+    L = torch.log_softmax(logits, dim=-1) # logits from scores
+    LL = torch.sum(L.gather(dim=-1, index=token_ids.unsqueeze(-1)).squeeze(-1).masked_fill_(M, 0.0), dim=-1)
+    nLL = LL/torch.sum(torch.bitwise_not(M), dim=-1) # normalized loglikelihood
+
+    for i, (T, P) in enumerate(zip(token_ids, pos)):
         # Prepare code as a formatted string.
-        tokens = processor.batch_decode(T)
+        tokens = processor.batch_decode(T, skip_special_tokens=True)
         for i, p in enumerate(P): tokens[p] = f"{{{i}}}" # turn it into an RV
-        C = ''.join(tokens)
+        C = utils.remove_code_affixes(''.join(tokens))
 
         # Prepare random variable names as a list.
         X = list(range(len(P)))
 
         # Prepare logits as a list of tensors.
+        L_supp = torch.log_softmax(L[i,P][:,supp[i]], dim=-1)
 
-        PP.append(program.Program(C, X, P))
+        PP.append(program.Program(C, X, L_supp))
 
-    return PP
+    return PP, nLL
 
 # Sketch of what the generation loop will look like
 def generate_probcode(model, tokenizer, input_ids, **gen_kwargs):

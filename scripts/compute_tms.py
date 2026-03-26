77eebfcfb8c5de836e0c6ec0667bb6d3c54600e8
Renato Lui Geh <renatogeh@gmail.com>
2026-03-25 17:15:36 -0700

    Limit memory usage when compute text match score

--- scripts/compute_tms.py
+++ scripts/compute_tms.py
@@ -1,10 +1,14 @@
-import argparse
+import argparse, resource
 from scripts.text_match_score import evaluate_single_example
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--generated_code", type=str, required=True)
     parser.add_argument("--ground_truth_code", type=str, required=True)
+
+    # Limit memory usage.
+    MAX_MEM_USAGE = 20_000_000_000
+    resource.setrlimit(resource.RLIMIT_AS, (MAX_MEM_USAGE, MAX_MEM_USAGE))
 
     args = parser.parse_args()
 
